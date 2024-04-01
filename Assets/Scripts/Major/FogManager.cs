a8c45a130b65fcc4e96305b82b4f156ac86bed57
Harris <79892199+kishimai@users.noreply.github.com>
2024-04-02 01:17:58 +0800

    Update new version

---
+++ Assets/Scripts/Major/FogManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogManager : MonoBehaviour
+{
+    public List<GameObject> FogOfWar = new List<GameObject>();
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
